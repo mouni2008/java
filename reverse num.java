70bffd4ab08a01eb0d1b11781af39f614dc9aa7d
mouni2008 <prakashsridharm@gmail.com>
2026-07-23 14:10:12 +0530

    Add files via upload

---
+++ reverse num.java
@@ -0,0 +1,13 @@
+
+public class Reverse {
+	public static void main(String[] args) {
+		 int a=12345;
+		 while(a>0) {
+			 int rev=a%10;
+			 System.out.print(rev+" ");
+			 a=a/10;
+		 }
+	}
+
+
+}
