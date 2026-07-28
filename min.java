537a0d632cbc9b228252098c5987765e78a20133
mouni2008 <prakashsridharm@gmail.com>
2026-07-28 16:07:17 +0530

    Add files via upload

---
+++ min.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        int[] arr=new int[size];
+        for(int i=0;i<size;i++)
+        arr[i]=sc.nextInt();
+        array(size,arr);
+    }
+    public static void array(int size,int[] arr){
+        int min=arr[0];
+        for(int i=1;i<size;i++){
+            if(arr[i]<min){
+                min=arr[i];
+            }
+        }
+        System.out.print(min);
+    }
+}
