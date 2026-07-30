4377ee1c5d58dca90002294dbdc15a7ae01def97
mouni2008 <prakashsridharm@gmail.com>
2026-07-30 22:21:31 +0530

    Add files via upload

---
+++ Duplicate values.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        int [] arr=new int [size];
+        for(int i=0;i<size;i++)
+        arr[i]=sc.nextInt();
+        unique(size,arr);
+    }
+    public static void unique(int size,int[] arr){
+        boolean[] visited=new boolean[size];
+        for(int i=0;i<size;i++){
+            if(visited[i]){
+                continue;
+            }
+            int count=1;
+            for(int j=i+1;j<size;j++){
+                if(arr[i]==arr[j]){
+                    visited[j]=true;
+                    count++;
+                }
+            }
+            if(count>1){
+                System.out.print(arr[i]);
+            }
+        }
+    }
+}
